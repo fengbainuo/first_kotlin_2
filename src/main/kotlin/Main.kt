241fbb6bc9c56b2b8d4a76abb0f34c1388bef29e
beni <benedek.vigh@gmail.com>
2022-10-22 13:26:01 +0200

    first commit and push

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,10 @@ fun main(args: Array<String>) {
     print("írd be a stringet amit szeretnél látni: ")
     val a = readln()
     println(a)
+    println(Short.MIN_VALUE)
+    println(Short.MAX_VALUE)
+    println(Byte.MIN_VALUE)
+    println(Byte.MAX_VALUE)
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
