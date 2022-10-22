0003f33f786761ca91026d2b5948bbdc17366479
beni <benedek.vigh@gmail.com>
2022-10-22 12:36:03 +0200

    first commit and push

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
-import java.util.*
-
 fun main(args: Array<String>) {
     println("Hello World!")
+    val a = readln()
+    println(a)
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
